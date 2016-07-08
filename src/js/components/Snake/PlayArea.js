1975bb7fe40fe538cc801f603adadff7c3b5e27f
RobertBowie <robertb1903@gmail.com>
2016-07-07 18:32:30 -0700

    Builds basic playarea dynamically

--- src/js/components/Snake/PlayArea.js
+++ src/js/components/Snake/PlayArea.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Row, Col, Grid } from 'react-bootstrap';
 
-const PlayArea = ({}) => {
+const PlayArea = ({board}) => {
+
   return (
-    <div>Play Area Here!</div>
+    <div>
+      <table>
+        <tbody>
+          {
+          board.map((row, i) => <tr key={i}>{row.map((cell, j) => <td key={j}>{cell}</td>)}</tr>)
+          }
+        </tbody>
+      </table>
+    </div>
   );
 }
 
--- src/js/routes/Snake.js
+++ src/js/routes/Snake.js
@@ -9,25 +9,64 @@ class Snake extends React.Component {
   constructor() {
     super()
 
+
     this.state = {
-      color: 'black'
+      color: 'black',
+      score: 0,
+      highScore: 0,
+      snakePos: [[2, 2], [3, 2], [4,2]],
+      foodPos: [],
+      boardSize: 10,
+      board: [],
+      direction: 'n'
     }
 
+    this.updateBoard = this.updateBoard.bind(this);
     this.changeColor = this.changeColor.bind(this);
   }
 
+  componentDidMount() {
+    const board = this.generateBoard(this.state.boardSize);
+    this.setState({board}, this.updateBoard);
+  }
+
   changeColor() {
-    this.setState({color: 'red'});
+    const color = this.state.color === 'black' ? 'red' : 'black';
+    this.setState({color});
+  }
+
+  updateBoard() {
+    let board = this.state.board;
+    const snakePos = this.state.snakePos;
+    snakePos.forEach(segment => {
+      board[segment[0]][segment[1]] = 1;
+    });
+    this.setState({board});
+  }
+
+  generateBoard(boardSize) {
+    let output = [];
+    for (let i = 0; i < boardSize; i++) {
+      output.push([]);
+      for (let j = 0; j < boardSize; j++) {
+        output[i].push(0);
+      }
+    }
+    return output;
   }
 
   render() {
     let style = {color: this.state.color}
+    const boardSize = this.state.boardSize;
+    const board = this.state.board;
+    const snakePos = this.state.snakePos;
+
     return (
       <div>
         <Grid>
           <Row className="show-grid">
             <Col xs={12} md={4}></Col>
-            <Col xs={12} md={4}><h1 onMouseOver={this.changeColor} style={style}>Snake</h1></Col>
+            <Col xs={12} md={4}><h1 onMouseEnter={this.changeColor} onMouseLeave={this.changeColor} style={style}>Snake</h1></Col>
             <Col xs={12} md={4}></Col>
           </Row>
 
@@ -39,7 +78,7 @@ class Snake extends React.Component {
 
           <Row className="show-grid">
             <Col xs={12} md={4}></Col>
-            <Col xs={12} md={4}><PlayArea /></Col>
+            <Col xs={12} md={4}><PlayArea board={board} snakePos={snakePos} /></Col>
             <Col xs={12} md={4}></Col>
           </Row>
 
